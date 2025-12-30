674c5b36d34daebeddb5dd10dd7a9c7c3f3fbadf
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-12-30 09:22:21 +0200

    hasDuplicate added

---
+++ hasDuplicae.py
@@ -0,0 +1,12 @@
+
+def hasDuplicate(list):
+    for i in range(len(list) - 1):
+        for j in range(len(list)):
+            if i != j and list[i] == list[j]:
+                return True
+    return False
+            
+
+list = [1,2,3,3]
+
+print(hasDuplicate(list))
