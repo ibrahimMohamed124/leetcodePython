5fa48aa4b643f012a193aef16e45118ea51a3da4
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2026-01-22 20:39:28 +0200

    encode and decode strings program added

--- Arrays&Hashing/encode&decodeStrings.py
+++ Arrays&Hashing/encode&decodeStrings.py
@@ -1,14 +1,31 @@
+from typing import List
+
 class Solution:
 
-    def encode(self, strs):
-        output = ""
-        for i in strs:
-            prefix= str(len(i) + "#")
-            output = prefix + strs[i]
-        return output
-        
-    def decode(self, s: str):
-        pass
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "😂" + s
+        return res
 
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        delimiter = "😂"
 
-    print(encode("ibrahim mohamed talaat"))
+        while i < len(s):
+            # find delimiter
+            j = s.find(delimiter, i)
+
+            # get length
+            length = int(s[i:j])
+
+            # get word
+            start = j + len(delimiter)
+            word = s[start : start + length]
+            res.append(word)
+
+            # move index
+            i = start + length
+
+        return res
